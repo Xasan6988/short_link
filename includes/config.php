8827e344bac10af751d04ec1e4a3dc7a8cb74760
Atryom <238136@mail.ru>
2021-10-26 17:33:16 +0300

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+  define ('SITE_NAME', "Cut your URL");
+  define ("HOST", "http://".$_SERVER['HTTP_HOST']."/short_link");
+
+  const DB_HOST = '127.0.0.1';
+  const DB_NAME = 'short_link';
+  const DB_USER = 'root';
+  const DB_PASS = 'root';
+?>
